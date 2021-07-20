89fc88e6902f5ff764aa0e22e7e1d64a8bf2844e
lktgt15 <lktgt15@gmail.com>
2021-07-20 21:48:20 +0900

    init

---
+++ webide2/src/main/java/com/webide2/Webide2Application.java
@@ -0,0 +1,13 @@
+package com.webide2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webide2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webide2Application.class, args);
+	}
+
+}
